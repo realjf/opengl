089457a9a809b6373a5ba950dd05a5398cad4477
realjf <real_jf@hotmail.com>
2021-05-07 17:11:01 +0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # opengl
+
+```sh
+# 查看opengl版本信息
+glxinfo | grep version
+
+```
--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get install glew-dev
+apt-get install -y libglew-dev libopengl-dev libx11-dev libxi-dev libxcursor-dev libxrandr-dev libxinerama-dev libglfw3-dev libxxf86vm-dev
 
 # yum install glew-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel
 
