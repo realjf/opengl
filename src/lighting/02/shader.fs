7a7504dbc379a40220339f94b60c87ef5f73a5d1
realjf <real_jf@hotmail.com>
2021-05-11 17:23:11 +0800

    updates

---
+++ src/lighting/02/shader.fs
@@ -0,0 +1,37 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+
+in vec3 FragPos;
+in vec3 Normal;
+
+void main()
+{
+    float ambientStrength = 0.3;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // 漫反射
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // 镜面强度
+    float specularStrength = 0.5;
+    // 计算视线方向向量
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    // 计算镜面分量
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
