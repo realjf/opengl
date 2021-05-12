4ca26bbd177b3804663a33f54e6a6795981e82ee
realjf <real_jf@hotmail.com>
2021-05-12 16:44:48 +0800

    updates

--- src/in-practice/2d-game/game.h
+++ src/in-practice/2d-game/game.h
@@ -3,6 +3,8 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <vector>
+#include "game_level.h"
 
 enum GameState {
     GAME_ACTIVE,
@@ -10,18 +12,27 @@ enum GameState {
     GAME_WIN
 };
 
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100, 20);
+// Initial velocity of the player paddle
+const GLfloat PLAYER_VELOCITY(500.0f);
+
 class Game
 {
 public:
     GameState State;
     GLboolean Keys[1024];
     GLuint Width,Height;
+    
     Game(GLuint width, GLuint height);
     ~Game();
     void Init();
     void ProcessInput(GLfloat dt);
     void Update(GLfloat dt);
     void Render();
+
+    std::vector<GameLevel> Levels;
+    GLuint                 Level;
 };
 
 #endif
